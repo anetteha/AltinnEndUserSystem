2e36fe4f33fa556419381df3482bfb2f573904a8
anetteha <anette.hagberg@gmail.com>
2016-09-20 10:47:35 +0200

    Request ok

--- BadASBusiness/BadASBusiness/Program.cs
+++ BadASBusiness/BadASBusiness/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using BadASBusiness.GiveMeSome;
 
 
 namespace BadASBusiness
@@ -14,7 +14,26 @@ namespace BadASBusiness
         {
             using (var sendForm = new GiveMeSome.IntermediaryInboundExternalBasicClient())
             {
-               var respons = sendForm.SubmitFormTaskBasic("9151", "Testinator123", null, null,null,null,null);
+                var respons = sendForm.SubmitFormTaskBasic("9151", "Testinator123", null, null, null, null,
+                    new FormTaskShipmentBE
+                    {
+                        Reportee = "910293826",
+                        ExternalShipmentReference = "12346789",
+                        FormTasks =
+                            new FormTask
+                            {
+                                ServiceCode = "3811",
+                                ServiceEdition = 201501,
+                                Forms = new List<Form>
+                                    {
+                                        new Form {Completed = false, DataFormatId = "1521", DataFormatVersion = 10634,
+                                            FormData ="<![CDATA[<melding xmlns='http://seres.no/xsd/Kursdomene/LånesøknadJME_M/2011' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'><Innsender><søker><fødselsnummer>aaaaaaaaaaa</fødselsnummer><navn>String</navn></søker></Innsender></melding>]]>",
+                                            EndUserSystemReference = "987654321"}
+                                    }.ToArray()
+                            }
+                    });
+
+                var test = respons.ReceiptText;
             }
         }
     }
