9e57f78ed0f696b27c1231380e486ec697a59b1e
InezNesheim <inez_marhaug@hotmail.com>
2016-09-20 10:12:59 +0200

    vi har respons

--- BadASBusiness/BadASBusiness/Program.cs
+++ BadASBusiness/BadASBusiness/Program.cs
@@ -5,12 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 
+
 namespace BadASBusiness
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (var sendForm = new GiveMeSome.IntermediaryInboundExternalBasicClient())
+            {
+               var respons = sendForm.SubmitFormTaskBasic("9151", "Testinator123", null, null,null,null,null);
+            }
         }
     }
 }
