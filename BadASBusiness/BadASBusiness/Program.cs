b2503fd31e9189998de41ca60c9306a6f3048780
Inez.Nesheim <inem@soprasteria.com>
2016-09-23 09:39:00 +0200

    authenticate please!

--- BadASBusiness/BadASBusiness/Program.cs
+++ BadASBusiness/BadASBusiness/Program.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using BadASBusiness.GiveMeSome;
 using BadASBusiness.HowYouDoin;
+using BadASBusiness.Authenticate;
 
 
 namespace BadASBusiness
@@ -30,21 +31,30 @@ namespace BadASBusiness
                                 Forms = new List<Form>
                                     {
                                         new Form {Completed = false, DataFormatId = "1521", DataFormatVersion = 10634,
-                                            FormData ="<melding xmlns='http://seres.no/xsd/Kursdomene/LånesøknadJME_M/2011' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'><Innsender><søker><fødselsnummer>aaaaaaaaaaa</fødselsnummer><navn>String</navn></søker></Innsender></melding>",
+                                            FormData ="<melding xmlns='http://seres.no/xsd/Kursdomene/LånesøknadJME_M/2011' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'><Innsender><søker><fødselsnummer>aaaaaaaaaaa</fødselsnummer><navn>Frau Knoblauch</navn></søker></Innsender></melding>",
                                             EndUserSystemReference = "987654321"}
                                     }.ToArray()
                             }
                     });
 
                 var receiptId = respons.ReceiptId;
-                
+
 
                 Thread.Sleep(200);
 
                 using (var receiptclient = new ReceiptExternalBasicClient())
                 {
-                    var receipt = receiptclient.GetReceiptBasic("9151", "Testinator123", new ReceiptSearchExternal {ReceiptId = receiptId });
+                    var receipt = receiptclient.GetReceiptBasic("9151", "Testinator123", new ReceiptSearchExternal { ReceiptId = receiptId });
+                }
 
+                using (var authenticate = new SystemAuthenticationExternalClient())
+                {
+                    var request = new AuthenticationChallengeRequestBE()
+                    {
+                        AuthMethod = ""
+                    };
+
+                    var auth = authenticate.GetAuthenticationChallenge(request);
                 }
             }
         }
