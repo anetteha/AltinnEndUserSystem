6ca1de6589a348bf0be689dfa79d1acef1b511fa
Inez.Nesheim <inem@soprasteria.com>
2016-09-27 10:23:03 +0200

    ServiceLibrary

---
+++ BadASBusiness/ServiceLibrary/ServerProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+
+namespace ServiceLibrary
+{    public class ServerProgram
+    {
+        static void Main(string[] arguments)
+        {
+            Type serviceType = typeof(OnlineBatchReceiverSoap);
+            ServiceHost myHost = new ServiceHost(serviceType);
+
+            using (myHost)
+            {
+                myHost.Open();
+                Console.WriteLine("Press enter to close..");
+                Console.ReadLine();
+                myHost.Close();
+
+            }
+        }
+    }
+}
