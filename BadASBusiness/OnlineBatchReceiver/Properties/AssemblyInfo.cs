b00f3aff0a7f4897359711df9eb86082d2f90274
Inez.Nesheim <inem@soprasteria.com>
2016-09-28 12:36:01 +0200

    opprydding

--- BadASBusiness/OnlineBatchReceiver/Properties/AssemblyInfo.cs
+++ BadASBusiness/OnlineBatchReceiver/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OnlineBatchReceiver")]
+[assembly: AssemblyTitle("Mottak")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OnlineBatchReceiver")]
+[assembly: AssemblyProduct("Mottak")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
