c13f8b3bccc91f207a53098601a353f4b767e6fd
anetteha <anette.hagberg@gmail.com>
2016-09-29 11:21:06 +0200

    Added test method

--- BadASBusiness/OnlineBatchReceiver/OnlineBatchReceiverSoap.asmx.cs
+++ BadASBusiness/OnlineBatchReceiver/OnlineBatchReceiverSoap.asmx.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Reflection;
 using System.Web.Services;
-using System.Xml.Linq;
+using System.Xml;
 using System.Xml.Schema;
 using System.Xml.Serialization;
 using log4net;
@@ -40,54 +40,52 @@ namespace OnlineBatchReceiver
             _logger.Debug("ReceiveOnlineBatchExternalAttachment Recieved from: " + username + " Batch: " + batch);
 
             // Authenticate username + passw
-            if (Authenticate(username, passwd))
+            if (!Authenticate(username, passwd))
             {
-                _logger.Debug("ReceiveOnlineBatchExternalAttachment, User Aithenticated");
-                // Verify batch vs. XSD (Schema verification)
-                if (!VerifyBatchSchema(batch))
-                {
-                    _logger.Debug("ReceiveOnlineBatchExternalAttachment Validation Failed");
-                    return Response(resultCodeType.FAILED_DO_NOT_RETRY);
-                }
-
-                try
-                {
-                    // Deserializing object 
-                    var serializer = new XmlSerializer(typeof(DataBatch));
-                    DataBatch result;
-
-                    using (TextReader reader = new StringReader(batch))
-                    {
-                        result = (DataBatch)serializer.Deserialize(reader);
-                    }
-
-                    // Saving payload to disk
-                    var path = Path.Combine(_filepath + "\\RecievedXml\\");
-                    Directory.CreateDirectory(path);
-
-                    var pathAndFile = Path.Combine(path, SafeFileName(
-                        DateTime.Now + "_" + username + "_" + receiversReference + "_" + sequenceNumber + ".xml"));
-                    var file = File.Create(pathAndFile);
-
-                    serializer.Serialize(file, result);
-                    file.Close();
-
-                    // TODO save attachments
-                }
-                catch (Exception ex)
-                {
-                    _logger.Error(ex);
-                    return ex.Message;
-                }
-
-                _logger.Debug("ReceiveOnlineBatchExternalAttachment Validated OK ");
-                return Response(resultCodeType.OK);
+                _logger.Debug("ReceiveOnlineBatchExternalAttachment Invalid request");
+                return Response(resultCodeType.FAILED_DO_NOT_RETRY);
+            }
+            _logger.Debug("ReceiveOnlineBatchExternalAttachment, User Aithenticated");
+            // Verify batch vs. XSD (Schema verification)
+            if (!ValidateBatchXml(batch))
+            {
+                _logger.Debug("ReceiveOnlineBatchExternalAttachment Validation Failed");
+                return Response(resultCodeType.FAILED_DO_NOT_RETRY);
             }
 
-            _logger.Debug("ReceiveOnlineBatchExternalAttachment Invalid request");
-            return Response(resultCodeType.FAILED_DO_NOT_RETRY);
-        }
+            try
+            {
+                // Deserializing object 
+                var serializer = new XmlSerializer(typeof(DataBatch));
+                DataBatch result;
 
+                using (TextReader reader = new StringReader(batch))
+                {
+                    result = (DataBatch)serializer.Deserialize(reader);
+                }
+
+                // Saving payload to disk
+                var path = Path.Combine(_filepath + "\\RecievedXml\\");
+                Directory.CreateDirectory(path);
+
+                var pathAndFile = Path.Combine(path, SafeFileName(
+                    Guid.NewGuid() + "_" + username + "_" + receiversReference + "_" + sequenceNumber + ".xml"));
+                var file = File.Create(pathAndFile);
+
+                serializer.Serialize(file, result);
+                file.Close();
+
+                // TODO save attachments as zip
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex);
+                return ex.Message;
+            }
+
+            _logger.Debug("ReceiveOnlineBatchExternalAttachment Validated OK ");
+            return Response(resultCodeType.OK);
+        }
 
         private bool Authenticate(string username, string password)
         {
@@ -95,15 +93,15 @@ namespace OnlineBatchReceiver
             return true;
         }
 
-        private bool VerifyBatchSchema(string batchXml)
+        private bool ValidateBatchXml(string batchXml)
         {
-            //Validerer skjema med xdocument
-            var result = true;
-            XDocument xdoc;
-
             try
             {
-                xdoc = XDocument.Parse(batchXml);
+                var doc = new XmlDocument();
+                doc.Schemas.Add("", _filepath + "/xsd/genericbatch.2013.06.xsd");
+                
+                doc.LoadXml(batchXml);
+                doc.Validate(xmlValidationEventHandler);
             }
             catch (Exception ex)
             {
@@ -111,26 +109,7 @@ namespace OnlineBatchReceiver
                 return false;
             }
 
-            //var serializer = new XmlSerializer(typeof(DataBatch));
-            //DataBatch data;
-
-            //using (TextReader reader = new StringReader(batchXml))
-            //{
-            //    data = (DataBatch)serializer.Deserialize(reader);
-            //}
-
-            // TODO verify xml validation
-
-            var schemaSet = new XmlSchemaSet();
-            schemaSet.Add("", _filepath + "/xsd/genericbatch.2013.06.xsd");
-
-            xdoc.Validate(schemaSet, (sender, e) =>
-            {
-                _logger.Debug("Validating xml request");
-                result = false;
-            });
-
-            return result;
+            return true;
         }
 
         private string Response(resultCodeType code)
@@ -148,18 +127,22 @@ namespace OnlineBatchReceiver
             return stringWriter.ToString();
         }
 
+        private static void xmlValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            switch (e.Severity)
+            {
+                case XmlSeverityType.Warning:
+                    throw new XmlSchemaValidationException();
+                case XmlSeverityType.Error:
+                    throw new XmlSchemaValidationException();
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
         private static string SafeFileName(string path)
         {
-            return path
-                .Replace("\\", "")
-                .Replace("/", "")
-                .Replace("\"", "")
-                .Replace("*", "")
-                .Replace(":", "")
-                .Replace("?", "")
-                .Replace("<", "")
-                .Replace(">", "")
-                .Replace("|", "");
+            return path.Replace("\\", "").Replace("/", "").Replace("\"", "").Replace("*", "").Replace(":", "").Replace("?", "").Replace("<", "").Replace(">", "").Replace("|", "");
         }
     }
 }
--- BadASBusiness/OnlineBatchRecieverTest/OnlineBatchReceiverSoapTest.cs
+++ BadASBusiness/OnlineBatchRecieverTest/OnlineBatchReceiverSoapTest.cs
@@ -10,10 +10,18 @@ namespace OnlineBatchRecieverTest
         public void ReceiveOnlineBatchExternalAttachment_Returns_FAILED_DO_NOT_RETRY_When_InvalidXml()
         {
             var target = new OnlineBatchReceiverSoap();
-
-            var result = target.ReceiveOnlineBatchExternalAttachment("user", "", "", 0, "", null);
+            var result = target.ReceiveOnlineBatchExternalAttachment("user", "", "", 0, "<bogus><xml>test</xml></bogus>", null);
             Assert.IsTrue(result.Contains(resultCodeType.FAILED_DO_NOT_RETRY.ToString()));
         }
 
+        [Test]
+        public void ReceiveOnlineBatchExternalAttachment_Returns_OK_When_ValidXml()
+        {
+            var target = new OnlineBatchReceiverSoap();
+
+            var result = target.ReceiveOnlineBatchExternalAttachment("user", "", "", 0, 
+                "<DataBatch schemaVersion=\"1.0\" batchReference=\"0\" previousReference=\"0\" receiverReference=\"f5c7892e-e4ff-4820-98eb-38641fb983b3\" timeStamp=\"2015-09-15T13:30:51\" formTasksInBatch=\"1\"><DataUnits><DataUnit reportee=\"03033140044\" archiveReference=\"AR98330\" archiveTimeStamp=\"2015-09-15T13:30:48.91\"><Approvers><Approver approverId=\"03033140044\" approvedTimeStamp=\"2015-09-15T13:30:48.833\" securityLevel=\"lessSensitive\" /></Approvers><FormTask><ServiceCode>3903</ServiceCode><ServiceEditionCode>150512</ServiceEditionCode><Form><DataFormatId>1243</DataFormatId><DataFormatVersion>10656</DataFormatVersion><Reference>39815</Reference><ParentReference>0</ParentReference><FormData><![CDATA[<Skjema xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:brreg=\"http://www.brreg.no/or\" xmlns:dfs=\"http://schemas.microsoft.com/office/infopath/2003/dataFormSolution\" xmlns:tns=\"http://www.altinn.no/services/ServiceEngine/ServiceMetaData/2009/10\" xmlns:q1=\"http://schemas.altinn.no/services/ServiceEngine/ServiceMetaData/2009/10\" xmlns:ns1=\"http://www.altinn.no/services/Register/ER/2009/10\" xmlns:ns2=\"http://schemas.altinn.no/services/Register/2009/10\" xmlns:ns3=\"http://www.altinn.no/services/2009/10\" xmlns:q3=\"http://www.altinn.no/services/common/fault/2009/10\" xmlns:ns4=\"http://schemas.microsoft.com/2003/10/Serialization/\" xmlns:my=\"http://schemas.microsoft.com/office/infopath/2003/myXSD/2010-02-11T08:42:27\" xmlns:xd=\"http://schemas.microsoft.com/office/infopath/2003\" skjemanummer=\"1243\" spesifikasjonsnummer=\"10656\" blankettnummer=\"RF-1117\" tittel=\"Klage på likningen\" gruppeid=\"5800\" etatid=\"NoAgency\"><Skattyterinfor-grp-5801 gruppeid=\"5801\"><info-grp-5802 gruppeid=\"5802\"></info-grp-5802><Kontakt-grp-5803 gruppeid=\"5803\"><KontaktpersonEPost-datadef-27688 orid=\"27688\">xxx@yyy.no</KontaktpersonEPost-datadef-27688><Samtykke_Skatt-datadef-30001>2</Samtykke_Skatt-datadef-30001><Samtykke_Folkeregister-datadef-30002>2</Samtykke_Folkeregister-datadef-30002></Kontakt-grp-5803><klagefrist-grp-5804 gruppeid=\"5804\"><KlageGjeldendeInntektsar-datadef-25455 orid=\"25455\">2014</KlageGjeldendeInntektsar-datadef-25455><KlagemeldingSendtInnenKlagefrist-datadef-25454 orid=\"25454\">Ja</KlagemeldingSendtInnenKlagefrist-datadef-25454></klagefrist-grp-5804></Skattyterinfor-grp-5801><klage-grp-5805 gruppeid=\"5805\"><spesifisering-grp-5836 gruppeid=\"5836\"><KlageSpesifisering-datadef-25457 orid=\"25457\">3t5q5</KlageSpesifisering-datadef-25457></spesifisering-grp-5836></klage-grp-5805></Skjema>]]></FormData></Form></FormTask></DataUnit></DataUnits></DataBatch>", null);
+            Assert.IsTrue(result.Contains(resultCodeType.OK.ToString()));
+        }
     }
 }
